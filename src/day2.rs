a08d9d05ba67ab1c3248048300ce74198849822c
Linus Unnebäck <linus@folkdatorn.se>
2023-12-03 16:25:01 +0100

    🎉 Add solution to Day 2 - Part 2

--- src/day2.rs
+++ src/day2.rs
@@ -95,7 +95,26 @@ pub fn part1(input: &str) -> u64 {
         .sum()
 }
 
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> u64 {
+    input
+        .split('\n')
+        .map(|line| {
+            let game = line.parse::<Game>().unwrap();
+
+            game.minimum_cubes_needed(Cube::Blue)
+                * game.minimum_cubes_needed(Cube::Green)
+                * game.minimum_cubes_needed(Cube::Red)
+        })
+        .sum()
+}
+
 #[test]
 fn test_part1() {
     assert_eq!(part1("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), 8);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), 2286);
 }
