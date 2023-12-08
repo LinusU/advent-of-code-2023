c1117b5826a2ae0edc070ce18d2cb5cdd1905965
Linus Unnebäck <linus@folkdatorn.se>
2023-12-09 00:01:17 +0100

    🎉 Add solution to Day 7 - Part 1

---
+++ src/day7.rs
@@ -0,0 +1,140 @@
+use std::{collections::HashMap, str::FromStr};
+
+trait Sorted {
+    fn sorted(self) -> Self;
+}
+
+impl<T: Sized, const COUNT: usize> Sorted for [T; COUNT]
+where
+    T: Ord,
+{
+    fn sorted(mut self) -> Self {
+        self.sort();
+        self
+    }
+}
+
+impl<T: Sized> Sorted for Vec<T>
+where
+    T: Ord,
+{
+    fn sorted(mut self) -> Self {
+        self.sort();
+        self
+    }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+enum Card {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
+}
+
+impl From<char> for Card {
+    fn from(c: char) -> Self {
+        match c {
+            'A' => Card::Ace,
+            'K' => Card::King,
+            'Q' => Card::Queen,
+            'J' => Card::Jack,
+            'T' => Card::Ten,
+            '9' => Card::Nine,
+            '8' => Card::Eight,
+            '7' => Card::Seven,
+            '6' => Card::Six,
+            '5' => Card::Five,
+            '4' => Card::Four,
+            '3' => Card::Three,
+            '2' => Card::Two,
+            _ => panic!("Invalid card: {}", c),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandKind {
+    FiveOfAKind,
+    FourOfAKind,
+    FullHouse,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
+}
+
+struct Hand([Card; 5]);
+
+impl Hand {
+    fn kind(&self) -> HandKind {
+        let mut counts = HashMap::new();
+
+        for card in &self.0 {
+            *counts.entry(card).or_insert(0usize) += 1;
+        }
+
+        let counts = counts.into_values().collect::<Vec<_>>().sorted();
+
+        match counts[..] {
+            [5] => HandKind::FiveOfAKind,
+            [1, 4] => HandKind::FourOfAKind,
+            [2, 3] => HandKind::FullHouse,
+            [1, 1, 3] => HandKind::ThreeOfAKind,
+            [1, 2, 2] => HandKind::TwoPair,
+            [1, 1, 1, 2] => HandKind::OnePair,
+            [1, 1, 1, 1, 1] => HandKind::HighCard,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl FromStr for Hand {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let cards = s
+            .chars()
+            .map(Card::from)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+
+        Ok(Self(cards))
+    }
+}
+
+#[aoc(day7, part1)]
+pub fn part1(input: &str) -> usize {
+    let input = input
+        .split('\n')
+        .map(|line| line.split_once(' ').unwrap())
+        .map(|(hand, bet)| (hand.parse::<Hand>().unwrap(), bet.parse::<usize>().unwrap()))
+        .map(|(hand, bet)| (hand.kind(), hand.0, bet))
+        .collect::<Vec<_>>()
+        .sorted();
+
+    input
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(rank, (_, _, bet))| (rank + 1) * bet)
+        .sum()
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(
+        part1("32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483"),
+        6440
+    );
+}
